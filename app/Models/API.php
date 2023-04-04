858c045e9b7b03aba46de81e7f6fc830b57fbc88
Gabriel Costa da Silva <20221101110085@ifrn.local>
2023-04-04 00:12:41 -0300

    changes

---
+++ app/Models/API.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class API extends Model
+{
+    use HasFactory;
+    protected $table ="a_p_i_s";
+    protected $fillable=[
+        'nome',
+        'sobrenome',
+        'idade',
+        'matricula',
+    ];
+}
